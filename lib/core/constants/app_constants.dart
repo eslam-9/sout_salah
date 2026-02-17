a0d31f5c2c2aaa3e0eeb983317e45b49402ea36e
eslam-9 <eslamage20@gmail.com>
2026-02-17 18:20:09 +0200

    add start date

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  // Start date of Ramadan 1446 (2025)
+  // Note: Adjust this date based on moon sighting
+  static final DateTime ramadanStartDate = DateTime(2026, 2, 17);
+}
